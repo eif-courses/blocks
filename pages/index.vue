5ef24f2c72002125f9a6a48ed76d399a01d88cfe
Marius <m.gzegozevskis@gmail.com>
2023-09-14 14:25:06 +0300

    fixed infinity middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <script setup>
-const {locale} = useI18n()
+import {storeToRefs} from 'pinia';
+import {useAuthStore} from '~/store/auth';
+
+const store = useAuthStore(); // use auth store
+
+const {authenticated, categories} = storeToRefs(useAuthStore()); // make authenticated state reactive
+
+const user = ref({
+  username: 'ma@gmail.com',
+  password: 'kosmosas',
+});
+const router = useRouter();
+
+
+
+function hello(str) {
+  alert(str)
+}
+
+onMounted(async () => {
+  await store.getCategories
+});
+
 </script>
 
 <template>
@@ -7,7 +29,15 @@ const {locale} = useI18n()
 
     <LangSwitcher/>
 
+    <ol v-for="cat in categories" :key="cat.id">
+      <li> {{ cat.name }}, {{cat.id}} <button @click="(store.$state.id = cat.id)">View more</button></li>
+    </ol>
+
+    <p>{{store.getCategoryById}}</p>
+
     <p>{{ $t('home.title') }}</p>
+
+
 
   </div>
 </template>
--- store/auth.ts
+++ store/auth.ts
@@ -5,15 +5,21 @@ interface UserPayloadInterface {
     password: string;
 }
 
+
+const BASE_URL = "https://onlinecourses-production.up.railway.app"
+
+
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         authenticated: false,
         loading: false,
+        categories: [],
+        id: 0,
     }),
     actions: {
         async authenticateUser({username, password}: UserPayloadInterface) {
             // useFetch from nuxt 3
-            const {data, pending}: any = await useFetch('https://onlinecourses-production.up.railway.app/login', {
+            const {data, pending}: any = await useFetch(`${BASE_URL}/login`, {
                 method: 'post',
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                 body: new URLSearchParams({
@@ -36,4 +42,38 @@ export const useAuthStore = defineStore('auth', {
             token.value = null; // clear the token cookie
         },
     },
+    getters: {
+
+        async getCategories(state) {
+
+            if (this.authenticated) {
+
+                const token = useCookie('token');
+                const jwt = {Authorization: `Bearer ${token.value}`};
+
+                const {data, pending}: any = await useFetch(`${BASE_URL}/api/v1/products/categories`, {
+                    method: 'get',
+                    headers: jwt
+                });
+                state.loading = pending;
+
+                if (data.value) {
+                    state.categories = data.value;
+                    return state.categories;
+                } else {
+                    this.authenticated = false;
+                }
+
+            } else {
+                return state.categories;
+            }
+
+        },
+        getCategoryById(state) {
+            return this.categories.find((category: any) => category.id === state.id);
+        },
+
+    },
+
+
 });
